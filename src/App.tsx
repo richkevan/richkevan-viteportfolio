82120acc66784dec72d9a6b792b55d67477374b9
rich-instantteams <100153306+rich-instantteams@users.noreply.github.com>
2023-02-14 22:23:06 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/app.css"
+
+const App = () => {
+
+  return (
+    <div id="main" className="w-screen-overflow h-screen-overflow">
+      <div style={{paddingTop: "3rem"}}>
+      <h1 className="title">Rich Kevan</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
